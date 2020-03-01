3464b648c17812952502eb347dffc1b3b94f76e3
Akmal Dzaky <akmaldzaky33@gmail.com>
2020-03-02 01:53:59 +0700

    checkpoint Sudah setengah jalan, sedang merubah ciphertext hasil perkalian dan mod 26 ke dalam alfabet dan dekripsi alfabet tersebut

--- get.php
+++ get.php
@@ -3,27 +3,96 @@
 use MathPHP\LinearAlgebra\MatrixFactory;
 
 require_once(__DIR__ . '/vendor/autoload.php');
-
-// $korensponden = new ArrayObject(array());
-// $i = 0;
-// foreach (range('A', 'Z') as $char) {
-//     $korensponden->append($char);
-//     $i++;
-// }
-
 $matrixArr = &$_GET['matrix']; // it will be a two dimenssion array having value as  matrix have
-$A = MatrixFactory::create($matrixArr);
-for ($i = 0; $i < $A->getM(); $i++) {
-    for ($j = 0; $j < $A->getN(); $j++) {
-        echo $A[$i][$j] . " ";
+$A = MatrixFactory::create($matrixArr); //buat matrix pada library
+
+function toPlaintext($res)
+{
+    $korensponden = array(); //untuk menyimpan alfabet
+    $i = 0;
+    $cipher = array();
+    foreach (range('a', 'z') as $char) {
+        array_push($korensponden, $char);
+        $i++;
     }
-    echo "<br>";
+    for ($i = 0; $i < count($res); $i++) {
+        for ($j = 0; $j < count($res); $j++) {
+            $temp = array_search($res[$i][$j], $korensponden);
+            array_push($cipher, $temp);
+            echo $cipher[$i][$j];
+        }
+    }
+}
+function korensponden($plainText)
+{
+    $lower = strtolower($plainText); //ubah ke lowercase semua
+    $plainArr = str_split($lower); //ubah string ke array
+    $korensponden = array(); //untuk menyimpan alfabet
+    $cipherText = array(); //untuk menyimpan cipertext
+    $i = 0;
+    foreach (range('a', 'z') as $char) {
+        array_push($korensponden, $char);
+        $i++;
+    }
+    foreach ($plainArr as $v) {
+        $temp = array_search($v, $korensponden); //mencari huruf di array alfabet
+        array_push($cipherText, $temp); //memasukan index huruf ke array jika ditemukan
+    }
+    return $cipherText;
 }
 
-$inv = $A->inverse();
-for ($i = 0; $i < $A->getM(); $i++) {
-    for ($j = 0; $j < $A->getN(); $j++) {
-        echo $A[$i][$j] . " ";
+function plaintextArr($cipherText)
+{
+    global $A;
+    $del = 404; //char tambahan yang nanti akan di hapus saat selesai dekripsi ada di index ke-26
+    do { //cek apakah jumlah index pada ciphertext bisa dikali dengan key, jika tidak maka akan ditambahkan char tambahan supaya dapat di enkripsi
+        array_push($cipherText, $del); //memasukan char tambahan
+    } while (count($cipherText) % $A->getM() != 0);
+    echo 'cipherText (404 adalah char tambahan) : ';
+    for ($i = 0; $i < count($cipherText); $i++) {
+        echo $cipherText[$i] . ' ';
     }
-    echo "<br>";
+    $div = array_chunk($cipherText, $A->getM()); //membagi array menjadi m bagian tergantung key
+    $N = $A->getM();
+    multiply($A, $div, $res);
+    echo '<br>key * cipherText : [';
+    for ($i = 0; $i < $N; $i++) {
+        echo '[';
+        for ($j = 0; $j < $N; $j++) {
+            echo ($res[$i][$j] . ' ');
+        }
+        echo ("]");
+    }
+    echo ']';
+    toPlaintext($res);
+}
+
+function multiply(&$mat1, &$mat2, &$res)
+{
+    global $A;
+    $N = $A->getM();
+    for ($i = 0; $i < $N; $i++) {
+        for ($j = 0; $j < $N; $j++) {
+            $res[$i][$j] = 0;
+            for ($k = 0; $k < $N; $k++) {
+                $res[$i][$j] += $mat2[$i][$k] * $mat1[$j][$k]; //terbaik wkwkwk
+                $res[$i][$j] %= 26;
+            }
+        }
+    }
+}
+
+if ($A->isInvertible() == false) { //cek apakah matrix bisa di inverse
+    header('Location: index.php?error=isSingular&Row=' . $A->getM() . '&Col=' . $A->getN());
+} else {
+    echo "Matrix key anda : [";
+    for ($i = 0; $i < $A->getM(); $i++) {
+        echo '[';
+        for ($j = 0; $j < $A->getN(); $j++) {
+            echo $A[$i][$j] . " ";
+        }
+        echo "]";
+    }
+    echo ']<br>';
+    plaintextArr(korensponden($_GET['plaintext']));
 }
--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-if (isset($_GET['Row']) && isset($_GET['Column'])) {
+if (isset($_GET['Row']) && isset($_GET['Col'])) {
     $row = &$_GET['Row'];
-    $col = &$_GET['Column'];
+    $col = &$_GET['Col'];
+    if ($row != $col) {
+        header('Location: index.php?error=isSquare');
+    }
+    if (isset($_GET['error'])) {
+        $error = &$_GET['error'];
+        if ($error == 'isSingular') {
+            echo '<a style="color: red;">Matrix anda tidak bisa di inverse</a> <br>';
+        } elseif ($error == 'plaintext') {
+            echo '<a style="color: red;">Plaintext mengandung angka</a> <br>';
+        }
+    }
 ?>
+    <a> nilai kunci (Harus bisa di inverse):</a>
     <form action="get.php" method="get">
         <table>
             <?php
@@ -13,14 +25,25 @@ if (isset($_GET['Row']) && isset($_GET['Column'])) {
                 }
                 echo '</tr>';
             } ?>
+            <img src="segitigaPascal.png"><br>
+            <a>Gunakan segitiga pascal sebagai acuan</a>
         </table>
+        Masukkan plaintext : <input type="text" name="plaintext"><br>
         <input type="submit" value="Submit">
         <button><a href="index.php" style="text-decoration: none;">Kembali</a></button>
     </form>
-<?php } else { ?>
+<?php } else {
+    if (isset($_GET['error'])) {
+        $error = &$_GET['error'];
+        if ($error == 'isSquare') {
+            echo '<a style="color: red;">Matrix anda bukan m×m</a> <br>';
+        }
+    } ?>
+    Buat matrix kunci <br>
+    masukkan ukuran kunci (harus m×m):
     <form action="index.php" method="get">
         <input type="number" name="Row">
-        <input type="number" name="Column">
+        <input type="number" name="Col">
         <input type="submit" value="Submit">
     </form>
 <?php } ?>
